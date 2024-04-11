696a5d5be0cbf23a0be0df9773de51723381b2c2
Pinky_Mangla <65814649+pinkymangla@users.noreply.github.com>
2024-04-11 17:05:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def upload_screenshot():
-    screenshot_data = request.json.get('screenshot')
+    # screenshot_data = request.json.get('screenshot')
 
-    # Decode Base64-encoded screenshot data
-    import base64
-    screenshot_bytes = base64.b64decode(screenshot_data)
+    # # Decode Base64-encoded screenshot data
+    # import base64
+    # screenshot_bytes = base64.b64decode(screenshot_data)
 
     # Save screenshot to file (or perform further processing)
     # with open('screenshot.png', 'wb') as f:
